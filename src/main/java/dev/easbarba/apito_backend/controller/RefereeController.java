5285554960b9f64637fff76bc7502b45fb26209e
easbarba <easbarba@outlook.com>
2022-12-20 13:58:53 -0300

    referee: partially update with patch

--- src/main/java/dev/easbarba/apito_backend/controller/RefereeController.java
+++ src/main/java/dev/easbarba/apito_backend/controller/RefereeController.java
@@ -2,10 +2,11 @@ package dev.easbarba.apito_backend.controller;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,33 +18,42 @@ import dev.easbarba.apito_backend.domain.Referee;
 import dev.easbarba.apito_backend.service.RefereeService;
 
 @RestController
-@RequestMapping(value = "/api/v1/referees")
+@RequestMapping(path = "/api/v1/referees", produces = "application/json")
+@CrossOrigin(origins = { "http://localhost:8080" })
 class RefereeController {
-    @Autowired
-    private RefereeService refereeService;
+  private RefereeService refereeService;
 
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Referee> create(@RequestBody Referee referee) {
-        return refereeService.save(referee);
-    }
+  public RefereeController(RefereeService refereeService) {
+    this.refereeService = refereeService;
+  }
 
-    @GetMapping
-    public ResponseEntity<Iterable<Referee>> getAll() {
-        return refereeService.findAll();
-    }
+  @PostMapping(consumes = "application/json", produces = "application/json")
+  public ResponseEntity<Referee> create(@RequestBody Referee referee) {
+    return refereeService.save(referee);
+  }
 
-    @GetMapping("{id}")
-    public ResponseEntity<Optional<Referee>> getOne(@PathVariable(value = "id") Long id) {
-        return refereeService.findById(id);
-    }
+  @GetMapping
+  public ResponseEntity<Iterable<Referee>> getAll() {
+    return refereeService.findAll();
+  }
 
-    @PutMapping("{id}")
-    public ResponseEntity<?> update(@RequestBody Referee referee, @PathVariable(value = "id") Long id) {
-        return refereeService.update(referee, id);
-    }
+  @GetMapping("{id}")
+  public ResponseEntity<Optional<Referee>> getOne(@PathVariable(value = "id") Long id) {
+    return refereeService.findById(id);
+  }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
-        return refereeService.delete(id);
-    }
+  @PutMapping("{id}")
+  public ResponseEntity<?> update(@PathVariable(value = "id") Long id, @RequestBody Referee referee) {
+    return refereeService.update(referee, id);
+  }
+
+  @PatchMapping("{id}")
+  public ResponseEntity<?> updatePartial(@PathVariable(value = "id") Long id, @RequestBody Referee patch) {
+    return refereeService.updatePartial(patch, id);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+    return refereeService.delete(id);
+  }
 }
--- src/main/java/dev/easbarba/apito_backend/service/RefereeService.java
+++ src/main/java/dev/easbarba/apito_backend/service/RefereeService.java
@@ -2,7 +2,6 @@ package dev.easbarba.apito_backend.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,8 +11,11 @@ import dev.easbarba.apito_backend.repository.RefereeRepository;
 
 @Service
 public class RefereeService {
-    @Autowired
     private RefereeRepository refereeRepository;
+
+    public RefereeService(RefereeRepository refereeRepository) {
+        this.refereeRepository = refereeRepository;
+    }
 
     public ResponseEntity<Referee> save(Referee referee) {
         Referee ref = refereeRepository.save(referee);
@@ -41,10 +43,30 @@ public class RefereeService {
         return ResponseEntity.status(HttpStatus.OK).body(refereeRepository.save(ref.get()));
     }
 
+    public ResponseEntity<?> updatePartial(Referee referee, Long id) {
+        var ref = refereeRepository.findById(id);
+
+        if (!ref.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Referee not found.");
+        }
+
+        var refFound = ref.get();
+
+        if (referee.getName() != null) {
+            refFound.setName(referee.getName());
+        }
+
+        if (referee.getState() != null) {
+            refFound.setState(referee.getState());
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(refereeRepository.save(refFound));
+    }
+
     public ResponseEntity<?> delete(Long id) {
         var ref = refereeRepository.findById(id).orElse(null);
         refereeRepository.delete(ref);
 
-        return ResponseEntity.status(HttpStatus.OK).body("Referee deleted successfully.");
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Referee deleted successfully.");
     }
 }
