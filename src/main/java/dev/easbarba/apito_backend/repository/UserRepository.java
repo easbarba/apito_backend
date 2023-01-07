811d5ae6408be84a58d53e567e99d50f377bac83
easbarba <easbarba@outlook.com>
2023-01-07 14:21:44 -0300

    referee: use domain path

--- src/main/java/dev/easbarba/apito_backend/repository/UserRepository.java
+++ src/main/java/dev/easbarba/apito_backend/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package dev.easbarba.apito_backend.repository;
 
-import dev.easbarba.apito_backend.domain.Referee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import dev.easbarba.apito_backend.domain.User;
+
 @Repository
-public interface RefereeRepository extends CrudRepository<Referee, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
